b4bfc283483adc517873fc32322eb4d381e7e111
Maxime Scholiers <maximescholiers@gmail.com>
2022-04-20 14:12:35 +0200

    veranderingen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,42 @@
 <template>
 <div>
-  
-        <div class="containter">
-            <div class="row">
-                <Item v-for="product in products"
-                :key="product.id"
-                :item="product"/>
-            </div>
-        </div>  
-  <section class="jumbotron text-center">
+  <section class="jumbotron text-center header-background-image">
         <div class="container">
-          <div class="d-flex"><h1 class="jumbotron-heading primary force-mr-2">EZ Volt</h1><h1 class="secondary">EZ Life</h1></div>
+          <div class="d-flex"><h1 class="jumbotron-heading primary force-mr-2 header-size">EZ Volt</h1><h1 class="secondary header-size">EZ Life</h1></div>
           <p class="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptas tenetur labore alias soluta debitis provident sed quidem! Fugiat voluptatem minima totam velit odio in, doloremque quia quisquam quaerat repellendus.</p>
         </div>
-      </section>
+  </section>
 
       <div class="album py-5">
         <div class="container">
           <div class="row">
             <div class="col-md-4">
               <div class="card mb-4 shadow bg-white jump">
-                <img class="card-img-top" src="../assets/batteryIcon.png" alt="Battery">
+                <div>
+                  <img class="card-img-top card-image-size" src="../assets/batteryIcon.png" alt="Battery" w="10px" h="10px">
+                </div>
                 <div class="card-body bg-light border-top">
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto obcaecati rem eaque molestias recusandae nam, consequatur porro quo excepturi quae laborum nostrum deserunt explicabo aut, sapiente in dolorum animi.</p>
+                  <p class="card-text">Our 2600mAh AA and AAA Lithium ion battery’s are our most famous products. The high demand for these keep us growing and innovating to make better battery’s. We do have a lot more to offer than these two.</p>
                 </div>
               </div>
             </div>
             <div class="col-md-4">
               <div class="card mb-4 shadow bg-white jump">
-                <img class="card-img-top" src="../assets/phoneIcon.png" alt="Phone">
+                <div>
+                  <img class="card-img-top card-image-size" src="../assets/phoneIcon.png" alt="Phone">
+                </div>
                 <div class="card-body bg-light border-top">
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio magnam, consequuntur, cupiditate dolore et dolores ja zeker meer tekst beetje gelijk qmldskfjsqldkfj mlqskd flmsqd fjkkmùsfldkg ùsflmdk gmùfdlk ùsdgk lsdùmls dfg Eaque.</p>
+                  <p class="card-text">The EZVOLT app is what makes separates us from other battery company’s. With helpful tools and details of the battery it makes your life easier to check up on your battery’s. </p>
                 </div>
               </div>
             </div>
             <div class="col-md-4">
               <div class="card mb-4 shadow bg-white jump">
-                <img class="card-img-top" src="../assets/leafIcon.png" alt="Leaf">
+                <div>
+                  <img class="card-img-top card-image-size" src="../assets/leafIcon.png" alt="Leaf">
+                </div>
                 <div class="card-body bg-light border-top">
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit id perspiciatis illum error reprehenderit quia quidem veritatis accusantium est suscipit rerum culpa laboriosam omnis, placeat architecto natus temporibus delectus quae.</p>
+                  <p class="card-text">We are proud to say that we only sell green battery’s. Thanks to the Lithium ion science, we can now recharge our battery’s and start to increase our carbon foot print. Our battery’s are also made out of ethical mined sources.</p>
                 </div>
               </div>
             </div>
